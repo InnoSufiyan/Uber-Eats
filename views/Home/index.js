fde349738002920c6036c56265cce211a23f01c1
inno sufiyan <innosufiyan@gmail.com>
2021-12-14 14:14:59 -0800

    search bar completed

--- views/Home/index.js
+++ views/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import Header from '../../components/Header'
+import SearchBar from '../../components/SearchBar'
 
 const Home = () => {
     return (
         <View style={{ backgroundColor: '#eee', height: '100%' }}>
             <View style={{backgroundColor : 'white', marginTop : 50, padding: 15}}>
                 <Header />
+                <SearchBar />
             </View>
         </View>
     )
